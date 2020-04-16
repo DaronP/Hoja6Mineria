deed811cb48cb92a0cde458ebd7a0079d3e15fe8
DaronP <jandresperez1@yahoo.com>
2020-04-16 15:36:26 -0600

    D U D A

--- HT6/Script.R
+++ HT6/Script.R
@@ -19,10 +19,18 @@ datos <- cbind(datos, dummy(datos$SaleCondition, verbose = T))
 
 corte <- sample(nrow(datos), nrow(datos)*0.7)
 
+train <- datos[corte,]
+test <- datos[-corte,]
+
+#------------------DESDE ACA TENGO DUDA----------------
+modelo <- glm(SaleCondition_normal~., data = train[,c()], family = binomial(), maxit=100)
 
 
 
-
+pred <- predict(modelo, newdata = test[], type = "response")
+prediccion<-ifelse(pred>=0.5,1,0)
+confusionMatrix(as.factor(test$SaleCondition_Normal),as.factor(prediccion))
+#------------------HASTA ACA------------------------------
 
 
 
